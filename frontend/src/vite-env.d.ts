1aae25728c35ea251b44e88d883eaa7ecca8a08c
Yin <yinjianchen02@gmail.com>
2026-03-02 18:53:21 -0500

    chore: remove pdf.workder.min.mjs

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -5,7 +5,3 @@ declare module '*?url' {
     export default fileUrlContent;
 }
 
-declare module 'pdfjs-dist/build/pdf.worker.min.mjs' {
-    const workerSrc: string;
-    export default workerSrc;
-}
